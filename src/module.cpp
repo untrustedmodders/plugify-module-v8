be2abe66fdd3aba755dcf4820ab53f999dae2e4c
qubka <nikita.ushakov0000@outlook.com>
2026-02-01 16:55:32 +0000

    fix: supress asserts in return set

--- src/module.cpp
+++ src/module.cpp
@@ -32,7 +32,6 @@ using namespace std::chrono_literals;
 #ifndef NOMINMAX
 #define NOMINMAX 1
 #endif
-#endif
 #include <windows.h>
 #include <winhttp.h>
 #else
@@ -758,7 +757,7 @@ namespace v8lm {
 				return plg::vec4{ *x, *y, *z, *w };
 			}
 		}
-		ThrowTypeError("An any argument not supports python type: {} for marshalling.", value);
+		ThrowTypeError("An any argument not supports js type for marshalling.", value);
 		return std::nullopt;
 	}
 
@@ -1331,21 +1330,46 @@ namespace v8lm {
 			case ValueType::Void:
 				break;
 			case ValueType::Bool:
+				ret.Set<bool>({});
+				break;
 			case ValueType::Char8:
+				ret.Set<char>({});
+				break;
 			case ValueType::Char16:
+				ret.Set<char16_t>({});
+				break;
 			case ValueType::Int8:
+				ret.Set<int8_t>({});
+				break;
 			case ValueType::Int16:
+				ret.Set<int16_t>({});
+				break;
 			case ValueType::Int32:
+				ret.Set<int32_t>({});
+				break;
 			case ValueType::Int64:
+				ret.Set<int64_t>({});
+				break;
 			case ValueType::UInt8:
+				ret.Set<uint8_t>({});
+				break;
 			case ValueType::UInt16:
+				ret.Set<uint16_t>({});
+				break;
 			case ValueType::UInt32:
+				ret.Set<uint32_t>({});
+				break;
 			case ValueType::UInt64:
+				ret.Set<uint64_t>({});
+				break;
 			case ValueType::Pointer:
-			case ValueType::Float:
-			case ValueType::Double:
-				// HACK: Fill all 8 byte with 0
 				ret.Set<uintptr_t>({});
+				break;
+			case ValueType::Float:
+				ret.Set<float>({});
+				break;
+			case ValueType::Double:
+				ret.Set<double>({});
 				break;
 			case ValueType::Function:
 				ret.Set<void*>(nullptr);
